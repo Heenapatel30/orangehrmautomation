87ad34b9fe534db1d97f31908f36003b0f71225d
Heenapatel30 <heena3096heena@gmail.com>
2023-11-28 12:33:04 +0530

    First Test commit

---
+++ orangehrm_automation/src/test/java/TestNG/TestNgAnnotation.java
@@ -0,0 +1,15 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNgAnnotation {
+
+    @Test
+    public void setUp(){
+        System.out.println("Test");
+
+
+
+    }
+
+}
