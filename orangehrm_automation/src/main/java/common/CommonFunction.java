677aca3d562cedc54e304383ae9abfd671c46110
Heenapatel30 <heena3096heena@gmail.com>
2023-11-29 11:14:01 +0530

    Added login method and testng.xml file

--- orangehrm_automation/src/main/java/common/CommonFunction.java
+++ orangehrm_automation/src/main/java/common/CommonFunction.java
@@ -1,5 +1,46 @@
 package common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class CommonFunction {
 
+  public static WebDriver launchBrowser(String browser){
+
+    WebDriver driver = null;
+
+    switch(browser){
+        case "chrome":
+          ChromeOptions chromeOptions = new ChromeOptions();
+         chromeOptions.addArguments("--remote-allow-origins=*");
+         chromeOptions.setBinary("D:\\Downloads\\chrome-win64\\chrome-win64\\chrome.exe");
+         driver = new ChromeDriver(chromeOptions);
+        break;
+        case "firefox" :
+            System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY,"D:\\Downloads\\geckodriver-v0.33.0-win64\\geckodriver.exe");
+             driver = new FirefoxDriver();
+        break;
+    }
+    return driver;
+  }
+
+  public static void elementToBeVisible(WebDriver driver, By by){
+      Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+      wait.until(ExpectedConditions.visibilityOf(driver.findElement(by)));
+
+  }
+
 }
+
+
--- orangehrm_automation/src/test/java/TestNG/TestNgAnnotation.java
+++ orangehrm_automation/src/test/java/TestNG/TestNgAnnotation.java
@@ -1,15 +1,27 @@
 package TestNG;
 
+import common.CommonFunction;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNgAnnotation {
-
-    @Test
+    WebDriver driver = CommonFunction.launchBrowser("chrome");
+    @BeforeTest
     public void setUp(){
-        System.out.println("Test");
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
-
-
+    }
+    @Test
+    public void logIn() throws InterruptedException {
+        Thread.sleep(2000);
+        CommonFunction.elementToBeVisible(driver,(By.name("username")));
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
 }
